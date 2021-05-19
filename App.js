b578591a627a2b0ff40da577391b3aec205ecf64
wandrilledioubate <74004697+wandrilledioubate@users.noreply.github.com>
2021-05-20 00:12:00 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from "@react-navigation/native"
 
-export default function App() {
+import Tabs from './navigation/tabs'
+import {Accueil,AccueilCalendrier,Automne, été,FicheProduit,Hiver,Printemps,Scan} from './screens'
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Let's go !</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        screen.Options={{
+          headerShown: false
+        }}
+        initialRouteName={"Accueil"}
+        <Stack.Screen name="Accueil" component={Tabs} />
+        <Stack.Screen name="accueilcalendrier" component={AccueilCalendrier} />
+        <Stack.Screen name="automne" component={Automne} />
+        <Stack.Screen name="été" component={été} />
+        <Stack.Screen name="ficheproduit" component={FicheProduit} />
+        <Stack.Screen name="hiver" component={Hiver} />
+        <Stack.Screen name="printemps" component={Printemps} />
+        <Stack.Screen name="scan" component={Scan} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
